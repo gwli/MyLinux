28658d2dd54fc15e358012f1f6558cc83dd20b03
Victor Li <gwli@outlook.com>
2018-09-28 14:21:43 +0800

    README.py

---
+++ experiments/memory_layout/README.py
@@ -0,0 +1,10 @@
+"""
+each time booting the layout almost the same.
+different machine the gap after 1M is different.
+add the RAM is always appending after 4G.
+"""
+data = [
+    ("1","ubuntu 1804 8G in Slot 1","Mem 0x100000(1M) - 0xc84cbfff(3.129684G)"),
+    ("2","ubuntu 1804 16G in Slot 1-3","Mem 0x100000(1M) - 0x985fafff(2.38084G)"),
+    ("3","ubuntu 1804 16G in Slot 1-3","Mem 0x100000(1M) - 0x985fafff(3.129684G)"),
+]
