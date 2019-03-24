b44e0301c04aea5f50eead297e1f835ac495a050
Victor <gangwei.li@outlook.com>
2019-03-24 21:14:40 +0800

    delete pngmath

--- source/conf.py
+++ source/conf.py
@@ -34,7 +34,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.pngmath',
     'sphinx.ext.graphviz',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
